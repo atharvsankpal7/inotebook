a7e19150c322c095f8bd917ff2812e13af556196
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2023-12-17 20:35:59 +0530

    user Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-const jwt =require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "ThisIsSecretKeyForToken"
+const JWT_SECRET = "ThisIsSecretKeyForToken";
 
 const validatorArray = [
     body("name", "the value of name should be atleast 5").isLength({ min: 5 }),
@@ -44,18 +44,66 @@ router.post("/createuser", validatorArray, async (request, response) => {
             password: await createSecurePassword(request.body.password),
             email: request.body.email,
         });
-        const data ={
-            user:{
-                id:user.id
-            }
-        }
-        const authToken = jwt.sign(data,JWT_SECRET)
-        
+        const data = {
+            user: {
+                id: user.id,
+            },
+        };
+        const authToken = jwt.sign(data, JWT_SECRET);
+
         // user creation success
-        response.json({authToken})
+        response.json({ authToken });
     } catch (err) {
         console.log(err.message);
         response.status(500).send("Error contacting database");
     }
 });
+
+// endpoint --> api/auth/login Authenticate a user. No login required
+router.post(
+    "/login",
+    [
+        body("email", "enter valid email").isEmail(),
+        body("password", "enter valid password").isLength({ min: 8 }),
+    ],
+    async (request, response) => {
+        //checking for validation errors
+        const errors = validationResult(request);
+        // if errors are there return
+        if (!errors.isEmpty()) {
+            return response.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = request.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return response
+                    .status(400)
+                    .json({ error: "Invalid credentials" });
+            }
+            const comparePassword = await bcrypt.compare(
+                password,
+                user.password
+            );
+            if (!comparePassword) {
+                return response
+                    .status(400)
+                    .json({ error: "Invalid credentials" });
+            }
+            const data = {
+                user: {
+                    id: user.id,
+                },
+            };
+            const authToken = jwt.sign(data, JWT_SECRET);
+
+            // user login success
+            response.json({ authToken });
+        } catch (err) {
+            response.status(500).send("Database connection failed");
+        }
+    }
+);
+
 module.exports = router;
