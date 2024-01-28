0c8b149756c5135fa01d439ffeae9d2e89845afc
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-01-28 23:50:17 +0530

    bug Fixes

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,12 @@ const cors = require("cors");
 connectToMongo();
 const app = express();
 const port = 5000;
-const ip = "172.16.4.2";
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/notes", require("./routes/notes"));
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port,ip, () => {
+app.listen(port, () => {
     console.log(`Listening the app at http://localhost:${port}`);
 });
